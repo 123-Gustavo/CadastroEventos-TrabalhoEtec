e63645e1e0b54746ba8aacb3ed6caf76cda17c2f
Gustavo <fariasgusgustavo@gmail.com>
2025-11-13 20:38:53 -0300

    Adicionar arquivos de projeto.

---
+++ CadastroEventos-TrabalhoEtec/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CadastroEventos_TrabalhoEtec
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CadastroEventos-TrabalhoEtec/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CadastroEventos_TrabalhoEtec")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CadastroEventos_TrabalhoEtec.Pages")]
